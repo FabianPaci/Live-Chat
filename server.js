178a34885b2de50b77475d08ed3030a872666876
FabianPaci <fabianpaci@gmail.com>
2021-03-17 12:39:37 +1100

    init server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 8000 || process.env.PORT;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
