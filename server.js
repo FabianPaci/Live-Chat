b298c2d946a1bef75d5fa5c47c9245c51b980440
FabianPaci <fabianpaci@gmail.com>
2021-03-17 17:29:07 +1100

    messages

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
 const PORT = 8000 || process.env.PORT;
+const io = socketio(server);
+
+// connections and disconnections
+io.on("connection", (socket) => {
+  socket.emit("message", "welcome to live chat!");
+
+  socket.broadcast.emit("message", "user has joined the chat");
+
+  socket.on("disconnect", () => {
+    io.emit("user has disconnected");
+  });
+});
+
+// listen for messages
+socket.on("message", (msg) => {
+  io.emit("message", msg);
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = io;
